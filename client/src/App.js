edf6d4983f4a63d3880ff611ae4be402dc67dbec
bryannadejesus <bryanna.dejesus@gmail.com>
2022-03-23 10:06:41 -0400

    enter key press

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,26 @@
-
-import React from "react";
+import React from 'react';
 import { hot } from 'react-hot-loader/root';
 import Book from './Book';
-import "./styles.css"
+import './styles.css';
 
 function App() {
-
   return (
     <>
-    <article className="header">
-    <div className="headers">
-            <h1>
-             Next Read App
-            </h1>
-            <h3>
-              Find your next read by author, genre, or keyword of interest.
-            </h3>
-      </div>
-      <div><img src="https://i.postimg.cc/9f0w5q3V/books.png" alt="bookshelf"/><img src="https://i.postimg.cc/9f0w5q3V/books.png" alt="bookshelf"/></div>
-    </article>
+      <article className='header'>
+        <div className='headers'>
+          <h1>Next Read</h1>
+          <h3>Find your next read by author, genre, or keyword of interest.</h3>
+        </div>
+        <div>
+          <img src='https://i.postimg.cc/9f0w5q3V/books.png' alt='bookshelf' />
+          <img src='https://i.postimg.cc/9f0w5q3V/books.png' alt='bookshelf' />
+        </div>
+      </article>
       <div>
         <Book />
       </div>
-  
-   
-      </>
-  )
+    </>
+  );
 }
-
 
 export default hot(App);
--- client/src/Book.js
+++ client/src/Book.js
@@ -1,74 +1,112 @@
-import React, { useEffect, useState } from "react"
-import axios from "axios";
-import "./styles.css"
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './styles.css';
 
 function Book() {
-  const [book, setBook] = useState("");
+  const [book, setBook] = useState('');
   const [result, setResult] = useState([]);
-  const [apiKey, setApiKey] = useState("AIzaSyDzE5gl0IrxmLYJuC_Dwf26Om2skuCG1us");
+  const [apiKey, setApiKey] = useState(
+    'AIzaSyDzE5gl0IrxmLYJuC_Dwf26Om2skuCG1us'
+  );
 
-  const [titleHeader, setTitleHeader] = useState("");
-  const [authorHeader, setAuthorHeader] = useState("");
-  const [descriptionHeader, setDescriptionHeader] = useState("");
+  const [titleHeader, setTitleHeader] = useState('');
+  const [authorHeader, setAuthorHeader] = useState('');
+  const [descriptionHeader, setDescriptionHeader] = useState('');
 
   function handleInput(event) {
     const book = event.target.value;
     setBook(book);
   }
 
-  function handleClick(event) {
-    axios.get("https://www.googleapis.com/books/v1/volumes?q="+book+"&key="+apiKey+"&maxResults=40")
-    .then(data => {
-
-      const res = data.data.items;
-      const randomBook = res[Math.floor(Math.random()*res.length)]
-      const bookInfo = randomBook.volumeInfo;
-      const { title } = bookInfo;
-      const { authors } = bookInfo;
-      const { thumbnail } = bookInfo.imageLinks;
-      const { description } = bookInfo;
-      const { previewLink } = bookInfo;
-      
-      
-      setResult([title, authors, thumbnail, description, previewLink, "Click the book image to be redirected to E-Book preview and purchase options."]);
-      
-      setTitleHeader("Title: ");
-      setAuthorHeader("Author: ");
-      setDescriptionHeader("Description: ")
-    })
-    .catch(error => console.log('Error in fetch request.'))
+  function handleEnter(event) {
+    if (event.keyCode === 13) {
+      handleClick();
+    }
   }
-  function buttonClear(){
+
+  function handleClick(event) {
+    axios
+      .get(
+        'https://www.googleapis.com/books/v1/volumes?q=' +
+          book +
+          '&key=' +
+          apiKey +
+          '&maxResults=40'
+      )
+      .then((data) => {
+        const res = data.data.items;
+        console.log(res);
+        const randomBook = res[Math.floor(Math.random() * res.length)];
+        const bookInfo = randomBook.volumeInfo;
+        const { title } = bookInfo;
+        const { authors } = bookInfo;
+        const { thumbnail } = bookInfo.imageLinks;
+        const { description } = bookInfo;
+        const { previewLink } = bookInfo;
+
+        setResult([
+          title,
+          authors,
+          thumbnail,
+          description,
+          previewLink,
+          'Click the book image to be redirected to E-Book preview and purchase options.',
+        ]);
+
+        setTitleHeader('Title: ');
+        setAuthorHeader('Author: ');
+        setDescriptionHeader('Description: ');
+      })
+      .catch((error) => console.log('Error in fetch request.'));
+  }
+  function buttonClear() {
     window.location.reload();
   }
 
-    return (
-      <>
-      <article className="book"> <div>
-         <input size="25" type="text" onChange={handleInput} placeholder="Search here..."/>
-      </div>
-    <div className="button">
-      <button id="searchButton" onClick={handleClick}>Search</button><button type="reset" onClick={buttonClear}>Clear</button>
-    </div>
-    <div className="title">
-      {titleHeader}{result[0]}
-    </div>
-    <div className="author">
-      {authorHeader}{result[1]}
-    </div>
-    <div className="image">  
-      <a target="_blank" href={result[4]}><img src= {result[2]} alt={result}/></a>
-    </div>
-    <div className="bookLine">
-      <h4>{result[5]}</h4>
-    </div>
-    <div className="description">
-      {descriptionHeader}{result[3]}
-    </div>
-    </article> 
-
+  return (
+    <>
+      <article className='book'>
+        {' '}
+        <div>
+          <input
+            size='25'
+            type='text'
+            onKeyDown={handleEnter}
+            onChange={handleInput}
+            placeholder='Search here...'
+          />
+        </div>
+        <div className='button'>
+          <button id='searchButton' onClick={handleClick}>
+            Search
+          </button>
+          <button type='reset' onClick={buttonClear}>
+            Clear
+          </button>
+        </div>
+        <div className='title'>
+          {titleHeader}
+          {result[0]}
+        </div>
+        <div className='author'>
+          {authorHeader}
+          {result[1]}
+        </div>
+        <div className='image'>
+          <a target='_blank' href={result[4]}>
+            <img src={result[2]} alt={result} />
+          </a>
+        </div>
+        <div className='bookLine'>
+          <h4>{result[5]}</h4>
+        </div>
+        <div className='description'>
+          {descriptionHeader}
+          {result[3]}
+        </div>
+      </article>
     </>
-    )
+  );
 }
 
 export default Book;
